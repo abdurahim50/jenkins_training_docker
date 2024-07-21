52712c2382042f08fed2b23dea119c089f9234f3
Abdurahim <yonghoabdurahim@gmail.com>
2024-07-21 11:39:40 -0500

    Change to 'text/html'

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const http = require('http');
+const chalk = require('chalk'); // Import the chalk package
+
 const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from Node.js!\n');
+  res.setHeader('Content-Type', 'text/html'); // Change to 'text/html'
+  res.end(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Hello Node.js</title>
+    </head>
+    <body style="background-color: lightblue; color: darkblue; font-family: Arial, sans-serif;">
+      <h1>Hello from Node.js!</h1>
+      <p>This is a colored response body with inline CSS.</p>
+    </body>
+    </html>
+  `);
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(chalk.bold(`Server running at http://${hostname}:${port}/`));
 });
 
